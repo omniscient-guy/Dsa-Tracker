215b783df8ab0e5c3002939b52564b894cc236d2
omniscient-guy <ojasvagarwal17@gmail.com>
2026-06-15 22:47:08 +0530

    Improved Menu and AddProblem function

--- src/src/DSATracker.java
+++ src/src/DSATracker.java
@@ -4,35 +4,36 @@ public class DSATracker {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         dsa problem = new dsa();
-        System.out.println("----DSA TRACKER----");
-        System.out.println("1.Add Problem");
-        System.out.println("2.Delete Problem");
-        System.out.println("3.Search Problem");
-        System.out.println("4.View All Problem");
-        System.out.println("5.Add Statistics");
-        System.out.println("");
-        System.out.println("Enter Choice:");
-        int choice = sc.nextInt();
-        switch(choice){
-            case 1 -> {
-                problem.AddProblem();
-            }
-            case 2 -> {
-                problem.DeleteProblem();
-            }
-            case 3 -> {
-                problem.SearchProblem();
-            }
-            case 4 -> {
-                problem.ViewAllProblem();
-            }
-            case 5 -> {
-                problem.ViewStatics();
-            }
-            case 6 -> {
-                System.out.println("THANK YOU FOR USING");
+        boolean running = true;
+        while(running==true){
+            System.out.print("----DSA TRACKER MENU----\n1.Add Problem\n2.Delete Problem\n3.Search Problem\n4.View All Problem\n5.Add Statistics\nEnter Choice:");
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1 -> {
+                    problem.AddProblem();
+                }
+                case 2 -> {
+                    problem.DeleteProblem();
+                }
+                case 3 -> {
+                    problem.SearchProblem();
+                }
+                case 4 -> {
+                    problem.ViewAllProblem();
+                }
+                case 5 -> {
+                    problem.ViewStatics();
+                }
+                case 6 -> {
+                    System.out.println("THANK YOU FOR USING");
+                    running = false;
+                }
+                default -> {
+                    System.out.println("----------INVALID CHOICE--PLEASE CHOOSE FROM ABOVE OPTIONS----------");
+                }
             }
         }
+
 
 
     }
--- src/src/dsa.java
+++ src/src/dsa.java
@@ -1,17 +1,33 @@
-import java.util.*:
+import java.util.*;
 public class dsa{
     public void AddProblem(){
         Scanner sc = new Scanner(System.in);
         System.out.print("How many problems do you want to add:");
         int count = sc.nextInt();
+        sc.nextLine();
         List<String> problems = new ArrayList<>();
         List<String> difficulty = new ArrayList<>();
         for(int i = 0 ; i<count ; i++){
             System.out.print("Enter Problem name:");
             problems.add(sc.nextLine());
-            System.out.print("Enter Difficulty level:");
-            difficulty.add(sc.nextLine());
-            System.out.println("");
+            System.out.print("Choose Difficulty:\n1.Low\n2.Medium\n3.High\nEnter Choice: ");
+            int choice =  sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1 -> {
+                    difficulty.add("Low");
+                }
+                case 2 -> {
+                    difficulty.add("Medium");
+                }
+                case 3 -> {
+                    difficulty.add("High");
+                }
+                default -> {
+                    System.out.println("Enter Choice From Above:");
+                }
+            }
+
         }
     }
     public void DeleteProblem(){
