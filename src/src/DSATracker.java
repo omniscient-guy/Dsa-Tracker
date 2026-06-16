0f4049579167ad9c67ab76920c39c531c83f2e24
omniscient-guy <ojasvagarwal17@gmail.com>
2026-06-16 23:30:53 +0530

    improved AddProblem

--- src/src/DSATracker.java
+++ src/src/DSATracker.java
@@ -6,7 +6,7 @@ public class DSATracker {
         dsa problem = new dsa();
         boolean running = true;
         while(running==true){
-            System.out.print("----DSA TRACKER MENU----\n1.Add Problem\n2.Delete Problem\n3.Search Problem\n4.View All Problem\n5.Add Statistics\nEnter Choice:");
+            System.out.print("----DSA TRACKER MENU----\n1.Add Problem\n2.Delete Problem\n3.Search Problem\n4.View All Problem\n5.Add Statistics\n6.Exit\nEnter Choice:");
             int choice = sc.nextInt();
             switch(choice){
                 case 1 -> {
@@ -29,7 +29,7 @@ public class DSATracker {
                     running = false;
                 }
                 default -> {
-                    System.out.println("----------INVALID CHOICE--PLEASE CHOOSE FROM ABOVE OPTIONS----------");
+                    System.out.println("----------INVALID CHOICE PLEASE CHOOSE FROM ABOVE OPTIONS----------");
                 }
             }
         }
--- src/src/dsa.java
+++ src/src/dsa.java
@@ -1,33 +1,46 @@
 import java.util.*;
 public class dsa{
+    List<String> problems = new ArrayList<>();
+    List<String> difficulty = new ArrayList<>();
     public void AddProblem(){
         Scanner sc = new Scanner(System.in);
         System.out.print("How many problems do you want to add:");
         int count = sc.nextInt();
         sc.nextLine();
-        List<String> problems = new ArrayList<>();
-        List<String> difficulty = new ArrayList<>();
         for(int i = 0 ; i<count ; i++){
             System.out.print("Enter Problem name:");
-            problems.add(sc.nextLine());
-            System.out.print("Choose Difficulty:\n1.Low\n2.Medium\n3.High\nEnter Choice: ");
-            int choice =  sc.nextInt();
-            sc.nextLine();
-            switch (choice) {
-                case 1 -> {
-                    difficulty.add("Low");
+            String name = sc.nextLine();
+            if(problems.contains(name)){
+                System.out.println("Problem Already Present Skipping...");
+                i--;
+                continue;
+            }
+            else{
+                problems.add(name);
+            }
+            boolean running = true;
+            while(running == true) {
+                System.out.print("Choose Difficulty:\n1.Low\n2.Medium\n3.High\nEnter Choice: ");
+                int choice =  sc.nextInt();
+                sc.nextLine();
+                switch (choice) {
+                    case 1 -> {
+                        difficulty.add("Low");
+                    }
+                    case 2 -> {
+                        difficulty.add("Medium");
+                    }
+                    case 3 -> {
+                        difficulty.add("High");
+                    }
+                    default -> {
+                        System.out.println("Enter Choice From Above:");
+                    }
                 }
-                case 2 -> {
-                    difficulty.add("Medium");
-                }
-                case 3 -> {
-                    difficulty.add("High");
-                }
-                default -> {
-                    System.out.println("Enter Choice From Above:");
+                if(choice>=1&&choice<=3){
+                    running = false;
                 }
             }
-
         }
     }
     public void DeleteProblem(){
